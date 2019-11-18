c6914a2e726517d3e8733702ee55a69c150d3a19
Matteo Balasso <balasoft.inc@gmail.com>
2019-11-18 02:06:44 +0100

    the chief and supervisor configuration updated

--- umano/services/thechief.py
+++ umano/services/thechief.py
@@ -2,6 +2,7 @@ import os
 
 import cv2
 import numpy as np
+import logging
 
 from umano.services.base import DataFetcher
 from umano.hal.data import last
@@ -20,32 +21,36 @@ class TheChief(DataFetcher):
         for hand_feature in self.data:
             self.log(msg="found hand {}".format(hand_feature.uid))
             hand_picture = last("OneHandPicture", query={"touch_session_id": hand_feature.touch_session_id})
-
+            if hand_picture is None:
+                continue
             if not os.path.exists(hand_picture.src):
                 continue
-            min_xy, max_xy = hand_roi(hand_feature)
-            frame = cv2.imread(hand_picture.src)
-            handprint = crop(hand_print(hand_picture, frame.shape[0], frame.shape[1]), min_xy, max_xy)
+            try:
+                min_xy, max_xy = hand_roi(hand_feature)
+                frame = cv2.imread(hand_picture.src)
+                handprint = crop(hand_print(hand_picture, frame.shape[0], frame.shape[1]), min_xy, max_xy)
 
-            output_file = os.path.join(
-                settings.ONEHAND_HAND_PRINTS_FOLDER,
-                "{}{}".format(hand_feature.uid, settings.ONEHAND_HAND_PRINTS_EXTENSION)
-            )
-            cv2.imwrite(output_file, handprint)
+                output_file = os.path.join(
+                    settings.ONEHAND_HAND_PRINTS_FOLDER,
+                    "{}{}".format(hand_feature.uid, settings.ONEHAND_HAND_PRINTS_EXTENSION)
+                )
+                cv2.imwrite(output_file, handprint)
 
-            features = np.zeros(shape=frame.shape)
-            hand = HandFeature(hand_feature.image_points)
-            annotate_frame_with_features(features, hand=hand)
-            output_file = os.path.join(
-                settings.ONEHAND_HAND_PRINTS_FOLDER,
-                "{}_features{}".format(hand_feature.uid, settings.ONEHAND_HAND_PRINTS_EXTENSION)
-            )
-            cv2.imwrite(output_file, crop(features, min_xy, max_xy))
-            hand_feature.width = handprint.shape[0]
-            hand_feature.height = handprint.shape[1]
-            hand_feature.hand_print = True
-            hand_feature.save()
-            self.log(msg="processed hand {}".format(hand_feature.uid))
+                features = np.zeros(shape=frame.shape)
+                hand = HandFeature(hand_feature.image_points)
+                annotate_frame_with_features(features, hand=hand)
+                output_file = os.path.join(
+                    settings.ONEHAND_HAND_PRINTS_FOLDER,
+                    "{}_features{}".format(hand_feature.uid, settings.ONEHAND_HAND_PRINTS_EXTENSION)
+                )
+                cv2.imwrite(output_file, crop(features, min_xy, max_xy))
+                hand_feature.width = handprint.shape[0]
+                hand_feature.height = handprint.shape[1]
+                hand_feature.hand_print = True
+                hand_feature.save()
+                self.log(msg="processed hand {}".format(hand_feature.uid))
+            except:
+                self.log(msg="critical error with hand {}".format(hand_feature.uid), level=logging.CRITICAL)
 
 if __name__ == "__main__":
     service = TheChief()
