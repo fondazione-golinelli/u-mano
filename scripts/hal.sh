62d8fbe240e2e72f27b9bc71f46eb41cabf640a6
mbalasso <balasoft.inc@gmail.com>
2019-11-22 16:56:04 +0100

    hal scripts

--- scripts/hal.sh
+++ scripts/hal.sh
@@ -4,3 +4,5 @@ source .bash_profile
 export PYTHONPATH=/Users/mini/projects/u-mano/:/Users/mini/projects/u-mano/umano
 cd /Users/mini/projects/u-mano/ || exit
 supervisord -c supervisor/hal.cfg
+cd /Users/mini/projects/multivisor/ || exit
+python -m multivisor.server.web -c /Users/mini3/projects/u-mano/supervisor/multivisor.conf
